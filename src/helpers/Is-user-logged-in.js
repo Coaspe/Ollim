8d4b62b6cc812ba57a6e846905970ecce632ea95
coaspe <aspalt85@gmail.com>
2022-03-27 09:04:29 +0900

    Is user logged in

--- src/helpers/Is-user-logged-in.js
+++ src/helpers/Is-user-logged-in.js
@@ -2,9 +2,9 @@ import { Navigate } from "react-router-dom";
 
 const IsUserLoggedIn = ({ children, user }) => {
   if (user !== null || user !== {}) {
-    return children;
+    return <Navigate to={`/${user.uid}`} />;
   }
-  return <Navigate to={`/${user.uid}`} />;
+  return children;
 };
 
 export default IsUserLoggedIn;
